e5187dc0f04fe128636e130928d387503ddee6e6
ADMichnik <amek995@gmail.com>
2023-10-24 10:16:53 +0200

    g c's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AssistanceComponent } from './components/assistance/assistance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    ContactComponent,
+    LandingComponent,
+    AssistanceComponent
   ],
   imports: [
     BrowserModule,
